58860445f23a0de84f4f11c6bcab88212d4f9b5d
Julie Teeuwen <julie.teeuwen@nn.nl>
2021-08-02 21:30:15 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+  },
+};
