eb7f27a78121cf90aa0521953986ddaae97170f8
Julie Teeuwen <julie.teeuwen@nn.nl>
2021-08-03 18:27:33 +0200

    publish pages

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:julie2305/amsterdam-birds.git master:gh-pages
+
+cd -
