275661efde9a5f11bbba3dd43f3132df24a0383a
Julie Teeuwen <julie.teeuwen@nn.nl>
2021-08-11 17:11:02 +0200

    Clear vue example stuff

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Map />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    Map
   }
 })
 </script>
---
+++ src/components/Map.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Map</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Map'
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
